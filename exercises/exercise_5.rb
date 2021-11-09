43482a9463548ba0c47f0ba3ac29c65b55d05d28
kenny-tse <kennytse91@gmail.com>
2021-11-09 05:57:14 +0000

    add had_many and belongs_to fields to store and eployee classes and add functionality to exercise 6

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+total = Store.sum('annual_revenue')
+
+num_of_stores = Store.count
+
+puts "Total annual revenue is #{total}"
+
+puts "Average annual revenue of all stores is #{total/num_of_stores}"
+
+million_or_more = Store.where('annual_revenue > 1000000').count
+
+puts "Number of stores with equal or more than 1 million annual revenue is #{million_or_more}"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Bob", last_name: "Miller", hourly_rate: 50)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 50)
+@store1.employees.create(first_name: "Eric", last_name: "Guy", hourly_rate: 55)
+
+@store2.employees.create(first_name: "Jane", last_name: "Vee", hourly_rate: 50)
+@store2.employees.create(first_name: "Tyler", last_name: "New", hourly_rate: 30)
+@store2.employees.create(first_name: "Ruth", last_name: "Ross", hourly_rate: 60)
+@store2.employees.create(first_name: "Mandy", last_name: "Li", hourly_rate: 50)
